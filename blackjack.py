ffd4403bb72ef5c41d4fab78d1ba990a26e97144
Adam Siwiec <acs0029@mail.wvu.edu>
2021-02-06 22:55:44 -0500

    clean up - still need to add ace functionality

--- blackjack.py
+++ blackjack.py
@@ -1,11 +1,11 @@
 import random
-import os
 from colorama import Fore, Back, Style, init
 from pip._vendor.distlib.compat import raw_input
 
 deck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]*4
 init(autoreset=True)
 highcards = ['A','J','Q','K']
+
 
 def deal(deck):
     hand = []
@@ -85,7 +85,7 @@ if __name__ == "__main__":
     playAgain = 'N'
     game = raw_input(Fore.LIGHTMAGENTA_EX + f'Hello, {Fore.CYAN + Style.BRIGHT + str(name)}.' + Fore.LIGHTMAGENTA_EX + Style.NORMAL + " Type 'S' to start and 'R' to run away: ").lower()
     if game == 's' or playAgain == 'y':
-        print('\nDealing...')
+        print(Fore.LIGHTMAGENTA_EX + '\nDealing...')
         while game == 's':
             dealerHand = deal(deck)
             playerHand = deal(deck)
@@ -97,10 +97,10 @@ if __name__ == "__main__":
             while decision == 'h':
                 playerHand.append(hit(deck))
                 playerTotal = normalize_hand(playerHand, total)
-                print(str(playerTotal))
-                print('The dealer shows:')
+                print("Your Total: " + str(playerTotal))
+                print(Fore.BLUE + 'The dealer shows:')
                 print(dealerHand[0])
-                print('Your hand:')
+                print(Fore.CYAN + 'Your hand:')
                 print_cards(playerHand)
                 if playerTotal == 21:  # Check for win
                     print(Fore.GREEN + "\nAuto-stand.\n")
@@ -109,7 +109,7 @@ if __name__ == "__main__":
                     print(Fore.RED + 'You busted!!')
                     decision = 's'
                 if playerTotal < 21:  # Prompt player to Hit/Stand
-                    decision = raw_input("\nType 'H' to Hit and 'S' to Stand: ").lower()
+                    decision = raw_input(Fore.LIGHTWHITE_EX + "\nType 'H' to Hit and 'S' to Stand: ").lower()
             if decision == 's':
 
                 while normalize_hand(dealerHand) < 17:
@@ -121,7 +121,7 @@ if __name__ == "__main__":
                 print(Fore.BLUE + 'Player')
                 print_cards(playerHand)
                 find_winner(dealerHand, playerHand)
-            playAgain = raw_input("Would you like to play again? (Y/N) ").lower()
+            playAgain = raw_input(Fore.LIGHTMAGENTA_EX + "Would you like to play again? (Y/N) ").lower()
 
 
 
