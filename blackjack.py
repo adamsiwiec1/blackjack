c5bcc232451e5a770676a1e6064a888d2309093a
Adam Siwiec <acs0029@mail.wvu.edu>
2021-02-06 21:04:32 -0500

    created loop for hit

--- blackjack.py
+++ blackjack.py
@@ -23,6 +23,19 @@ def deal(deck):
     return hand
 
 
+def hit(deck):
+    card = deck.pop()  # The pop() method removes the item at the given index from the list and returns the removed item
+    if card == 1:
+        card = 'A'
+    if card == 11:
+        card = 'J'
+    if card == 12:
+        card = 'Q'
+    if card == 13:
+        card = 'K'
+    return card
+
+
 if __name__ == "__main__":
     print("Hello, Welcome to Blackjack.\n\n")
     name = raw_input("Enter your name to begin: ")
@@ -36,7 +49,19 @@ if __name__ == "__main__":
             print(f"The dealer shows:\n{dealerHand[0]}")
             print(f'Here are your cards:\n{playerHand[0]}\n{playerHand[1]}')
 
-            decision = raw_input("Type 'H' to hit and 'S' to stand.")
+            decision = raw_input("Type 'H' to hit and 'S' to stand.").lower()
+            while decision == 'h':
+                playerHand.append(hit(deck))
+                print(f"The dealer shows:\n{dealerHand[0]}\n{dealerHand[1]}")
+                print(f'Your cards:\n')
+                for card in playerHand:
+                    print(f'{card} \n')
+                decision = raw_input("Type 'H' to hit and 'S' to stand.").lower()
+            if decision == 's':
+                print(f"The dealer shows:\n{dealerHand[0]}\n{dealerHand[1]}")
+                print(f'Your cards:\n{playerHand[0]}\n{playerHand[1]}')
+
+            playAgain = raw_input("Would you like to play again?")
 
 
 
